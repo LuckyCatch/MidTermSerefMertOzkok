93338e23bf473fcf7c391ab81bfad2137031d630
LuckyCatch <61746492+LuckyCatch@users.noreply.github.com>
2021-04-10 10:51:29 +0300

    Update index.php
    for the 6th question.

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+  
+/*
 <?php
 echo "Welcome to my GitHub page";
 ?>
+*/
 
+<form action="index.php" method="post">
+ <p>Licence Plate: <input type="text" name="LicencePlate" /></p>
+ <p>Brand: <input type="text" name="Brand" /></p>
+ <p>Model: <input type="text" name="Model" /></p>
+ <p>Model year: <input type="text" name="ModelYear" /></p>
+ <p>Color: <input type="text" name="Color" /></p>
+ <input type="submit">
+</form>
+  
 </body>
 </html>
